83f05baa0223c4f1c833f2ddc9ba180b75b7c6de
connor <connor.botterill@yahoo.co.uk>
2022-08-09 09:14:48 +0100

    implement full header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="pyramid__header section__padding" id="home">
+      <div className="pyramid__header-content">
+        <h1 className="gradient__text">
+          Supreme and Palace releases have never been easier.
+        </h1>
+        <p>
+          A new and upcoming software developed with a passion to make your life
+          easier with releases. No more frantic clicking and typing, we've got
+          it covered.
+        </p>
+        <div className="pyramid__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Register Interest</button>
+        </div>
+        <div className="pyramid__header-content__people">
+          <img src={people} alt="people" />
+          <p>1600 people requested access in the last 24 hours.</p>
+        </div>
+      </div>
+      <div className="pyramid__header-image">
+        <img src={ai} alt="ai" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
