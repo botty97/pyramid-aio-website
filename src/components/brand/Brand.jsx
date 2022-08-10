5109e47f049f1e5396496ed116f67184fa0e79f9
connor <connor.botterill@yahoo.co.uk>
2022-08-10 08:55:42 +0100

    implement branding for sites that we hit

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./brand.css";
+import { palace, supreme, shopify } from "./import.js";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="pyramid__brand section__padding">
+      <div>
+        <img src={supreme} alt="supreme" />
+      </div>
+      <div>
+        <img src={palace} alt="palace" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
---
+++ src/components/brand/import.js
@@ -0,0 +1,5 @@
+import supreme from "../../assets/supreme.png";
+import palace from "../../assets/palace.png";
+import shopify from "../../assets/shopify.png";
+
+export { palace, supreme, shopify };
