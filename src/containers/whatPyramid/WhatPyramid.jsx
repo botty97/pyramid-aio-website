bbf727106ba87aa73911c080d43e149ba224839d
connor <connor.botterill@yahoo.co.uk>
2022-08-10 20:08:00 +0100

    add features placeholder

--- src/containers/whatPyramid/WhatPyramid.jsx
+++ src/containers/whatPyramid/WhatPyramid.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "./whatPyramid.css";
+import { Feature } from "../../components";
 
 const WhatPyramid = () => {
-  return <div>WhatPyramid</div>;
+  return (
+    <div className="pyramid__whatpyramid section__margin" id="wpyramid">
+      <div className="pyramid__whatpyramid-feature">
+        <Feature
+          title="What is Pyramid"
+          text="THIS IS DUMMY TEXT TO EXPLAIN WHAT PYRAMID IS WILL BE DONE LATER"
+        />
+      </div>
+      <div className="pyramid__whatpyramid-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Join The Discord</p>
+      </div>
+      <div className="pyramid__whatpyramid-container">
+        <Feature
+          title="Community"
+          text="THIS IS DUMMY TEXT TO EXPLAIN WHAT PYRAMID IS WILL BE DONE LATER"
+        />
+        <Feature
+          title="Evolving"
+          text="THIS IS DUMMY TEXT TO EXPLAIN WHAT PYRAMID IS WILL BE DONE LATER"
+        />
+        <Feature
+          title="Reliable"
+          text="THIS IS DUMMY TEXT TO EXPLAIN WHAT PYRAMID IS WILL BE DONE LATER"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default WhatPyramid;
