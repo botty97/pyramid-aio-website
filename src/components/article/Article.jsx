04f0a295c2762ab857c5e48c1784328852503dad
connor <connor.botterill@yahoo.co.uk>
2022-08-11 20:43:09 +0100

    feat: implement faq section

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./article.css";
 
-const Article = () => {
-  return <div>Article</div>;
-};
+const Article = ({ imgUrl, text, answers }) => (
+  <div className="pyramid__faq-container_article">
+    {/* <div className="pyramid__faq-container_article-image">
+      <img src={imgUrl} alt="FAQ_image" /> //this needs to be a question mark will sort later
+    </div> */}
+    <div className="pyramid__faq-container_article-content">
+      <div>
+        <h3>{text}</h3>
+      </div>
+      <p>{answers}</p>
+    </div>
+  </div>
+);
 
 export default Article;
---
+++ src/containers/faq/Faq.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Article from "../../components/article/Article";
+import "./faq.css";
+
+const Faq = () => (
+  <div className="pyramid__faq section__padding" id="faq">
+    <div className="pyramid__faq-heading">
+      <h1 className="gradient__text">FAQ</h1>
+    </div>
+    <div className="pyramid__faq-container">
+      <div className="pyramid__faq-container_group">
+        <Article
+          text="How much is a license?"
+          answers="A license will be free at the beginning whilst the software is being beta tested."
+        />
+        <Article
+          text="Is it lifetime or a monthly renewal?"
+          answers="There will be a renewal fee of £20 per month for Pyramid AIO."
+        />
+        <Article
+          text="What sites are supported?"
+          answers="Currently, Palace and Supreme are supported. With more shopify sites being added in the near future."
+        />
+        <Article
+          text="When is release?"
+          answers="This is unconfirmed at the moment whilst we still work on the development of the software. Follow our Twitter to keep up to date with the progress."
+        />
+      </div>
+    </div>
+  </div>
+);
+
+export default Faq;
