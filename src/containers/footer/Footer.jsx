bea1ad3a5bd252076ffa5317835ace5123a06fbc
connor <connor.botterill@yahoo.co.uk>
2022-08-13 11:39:48 +0100

    feat: add footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 import "./footer.css";
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+const Footer = () => (
+  <div className="pyramid__footer section__padding">
+    <div className="pyramid__footer-links">
+      <div className="pyramid__footer-links_div">
+        <h4>Links</h4>
+        <p>Privacy Policy</p>
+        <p>Terms of Service</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="pyramid__footer-links_div">
+        <h4>Credit and Debit Cards Accepted</h4>
+        <p>VISA </p>
+        <p>Mastercard</p>
+        <p>American Expresss</p>
+      </div>
+      <div className="pyramid__footer-links_div">
+        <h4>Contact</h4>
+        <p>Twitter</p>
+        <p>Instagram</p>
+        <p>Email</p>
+      </div>
+    </div>
+
+    <div className="pyramid__footer-copyright">
+      <p>@2022 Pyramid. All rights reserved.</p>
+    </div>
+  </div>
+);
 
 export default Footer;
