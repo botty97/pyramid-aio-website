a2c6d3b77de87b2478640cd8a0f6c6533b8c7c8c
connor <connor.botterill@yahoo.co.uk>
2022-08-13 11:48:31 +0100

    chore: remove unnecessary files

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,5 +2,4 @@ export { default as Faq } from "./faq/Faq";
 export { default as Features } from "./features/Features";
 export { default as Footer } from "./footer/Footer";
 export { default as Header } from "./header/Header";
-export { default as Possibility } from "./possibility/Possibility";
 export { default as WhatPyramid } from "./whatPyramid/WhatPyramid";
