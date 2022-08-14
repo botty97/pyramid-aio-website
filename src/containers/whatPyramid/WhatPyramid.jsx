b6da2491bebb7cb926b3073c5b25f935f2ef63da
connor <connor.botterill@yahoo.co.uk>
2022-08-14 12:59:14 +0100

    chore: replace some dunny text in what is pyramid page

--- src/containers/whatPyramid/WhatPyramid.jsx
+++ src/containers/whatPyramid/WhatPyramid.jsx
@@ -8,12 +8,13 @@ const WhatPyramid = () => {
       <div className="pyramid__whatpyramid-feature">
         <Feature
           title="What is Pyramid"
-          text="THIS IS DUMMY TEXT TO EXPLAIN WHAT PYRAMID IS WILL BE DONE LATER"
+          text="Pyramid is an up and coming bot to make users life easier. The intention behind the bot is to guarantee success on sites such as Palace, Supreme and various shopify websites. We want our users to be the first to cop, and never have any worries with releases. This is a CLI bot with future intentions to make a frontend GUI for a better user experience."
         />
       </div>
       <div className="pyramid__whatpyramid-heading">
         <h1 className="gradient__text">
-          Enter now to experience constant success
+          Enter now to get involved with an ever growing community <br />
+          and reap the rewards of constant guidance and success.
         </h1>
         <p>
           <a
