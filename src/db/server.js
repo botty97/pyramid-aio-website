6e915c9e140a5a7d8b71990c7f96f82b5c694e30
connor <connor.botterill@yahoo.co.uk>
2022-08-17 20:12:05 +0100

    feat: change server to be correct

--- src/db/server.js
+++ src/db/server.js
@@ -1,35 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const path = require("path");
+const cors = require("cors");
 // eslint-disable-next-line no-unused-expressions
 require("dotenv").config;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect(String(process.env.DB_CONNECTION));
 
-mongoose.connect(process.env.DB_CONNECTION);
+app.use("/", require("../routes/emailRoute"));
 
-const emailSchema = {
-  email: String,
-};
-
-const Email = mongoose.model("Email", emailSchema);
-
-app.post("/testingAPI", (req, res) => {
-  console.log(req.body);
-  let newEmail = new Email({
-    email: req.body.email,
-  });
-  newEmail.save();
-  res.redirect("/testingAPI");
-});
-
-// app.get("/testingAPI", (req, res) => {
-//   res.sendFile(path.resolve("./index.html"));
-// });
-
-app.listen(3000, () => {
-  console.log("We are online!");
+app.listen(3001, () => {
+  console.log("The backend is now up and running on PORT 3001");
 });
