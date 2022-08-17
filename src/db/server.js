6099b6e174a3f36fe897af8d2e62c6a0b8090d64
connor <connor.botterill@yahoo.co.uk>
2022-08-17 09:13:20 +0100

    feat: flesh out more of the db

--- src/db/server.js
+++ src/db/server.js
@@ -1,10 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+// eslint-disable-next-line no-unused-expressions
+require("dotenv").config;
 
 const app = express();
 
-app.get("/testingAPI", (req, res) => {
-  res.send("Connection successful");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.DB_CONNECTION);
+
+const emailSchema = {
+  email: String,
+};
+
+const Email = mongoose.model("Email", emailSchema);
+
+app.post("/testingAPI", (req, res) => {
+  console.log(req.body);
+  let newEmail = new Email({
+    email: req.body.email,
+  });
+  newEmail.save();
+  res.redirect("/testingAPI");
 });
+
+// app.get("/testingAPI", (req, res) => {
+//   res.sendFile(path.resolve("./index.html"));
+// });
 
 app.listen(3000, () => {
   console.log("We are online!");
