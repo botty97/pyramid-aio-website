3d0a0523f41a24ae8a51656a31c59758a53ba67e
connor <connor.botterill@yahoo.co.uk>
2022-08-21 11:06:33 +0100

    chore: fix dotenv config

--- src/db/server.js
+++ src/db/server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 // eslint-disable-next-line no-unused-expressions
-require("dotenv").config;
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(String(process.env.DB_CONNECTION));
+mongoose.connect(process.env.DB_CONNECTION);
 
 app.use("/", require("../routes/emailRoute"));
 
