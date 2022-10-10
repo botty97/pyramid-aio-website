cfe13a9f5e14a97f32b11b5af79c0badd02074be
connor <connor.botterill@yahoo.co.uk>
2022-10-10 08:42:57 +0100

    chore: testing logs

--- src/routes/emailRoute.js
+++ src/routes/emailRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Email = require("../db/emailModel");
 
 router.route("/create").post((req, res) => {
+  console.log('did we fire?')
   const email = req.body.email;
 
   const newEmail = new Email({
