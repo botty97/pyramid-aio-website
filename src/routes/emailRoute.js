0bf298e88e8451b125c27a2b71d6724915fb688a
connor <connor.botterill@yahoo.co.uk>
2022-10-10 08:54:12 +0100

    chore: remove log

--- src/routes/emailRoute.js
+++ src/routes/emailRoute.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const Email = require("../db/emailModel");
 
 router.route("/create").post((req, res) => {
-  console.log('did we fire?')
   const email = req.body.email;
 
   const newEmail = new Email({
