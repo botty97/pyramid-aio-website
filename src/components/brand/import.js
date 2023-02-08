8c40e582e7bedfd4d1502896587687fdfd7fd03f
connor <connor.botterill@yahoo.co.uk>
2023-02-08 08:21:13 +0000

    refactor: remove unused imports

--- src/components/brand/import.js
+++ src/components/brand/import.js
@@ -1,9 +1,6 @@
-import supreme from "../../assets/supreme.png";
-import palace from "../../assets/palace.png";
-import shopify from "../../assets/shopify.png";
 import end from "../../assets/end.png";
 import dsm from "../../assets/dsm.png";
 import nakedcph from "../../assets/nakedcph.png";
 import footpatrol from "../../assets/footpatrol.png";
 
-export { palace, supreme, shopify, end, dsm, nakedcph, footpatrol };
+export { end, dsm, nakedcph, footpatrol };
