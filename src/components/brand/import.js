207c94059d7bb15ebca69ef55c4c0254c9cf1498
connor <connor.botterill@yahoo.co.uk>
2023-02-11 12:08:27 +0000

    feat: implement afew branding=

--- src/components/brand/import.js
+++ src/components/brand/import.js
@@ -2,5 +2,6 @@ import end from "../../assets/end.png";
 import dsm from "../../assets/dsm.png";
 import nakedcph from "../../assets/nakedcph.png";
 import footpatrol from "../../assets/footpatrol.png";
+import afew from "../../assets/afew.png";
 
-export { end, dsm, nakedcph, footpatrol };
+export { end, dsm, nakedcph, footpatrol, afew };
